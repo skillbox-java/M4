ffe3dfe5833ae6c09ac374b926d4fa50e441f9d8
Shibkov Konstantin <shibkov.k@gmail.com>
2020-01-16 02:58:29 +0500

    4.6 Домашняя работа 4.1

---
+++ 1/IntegerExperiments/src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args){
+        Container container = new Container();
+        container.count += 7843;
+
+    }
+
+    public static Integer sumDigits(Integer number){
+        //@TODO: write code here
+        return 0;
+    }
+}
